200f6b799ffe9105e059ad4fa0c8d662f1f1bc6a
unknown <rajagrawalmohit2@gmail.com>
2021-09-27 21:00:12 +0530

    Server Deploy

--- services/mailService.js
+++ services/mailService.js
@@ -1,21 +1,47 @@
 const nodemailer = require("nodemailer");
 module.exports = async ({ from, to, subject, text, html }) => {
-  let transporter = nodemailer.createTransport({
-    host: "smtp-relay.sendinblue.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+  //   let transporter = nodemailer.createTransport({
+  //     service: "gmail",
+  //     auth: {
+  //       user: "agrawalmohit.tech@gmail.com", // generated ethereal user
+  //       pass: "HLn9YOswp2Wx85C4", // generated ethereal password
+  //     },
+  //   });
+
+  //   // send mail with defined transport object
+  //   let info = await transporter.sendMail({
+  //     from: `inShare <${from}>`, // sender address
+  //     to: to, // list of receivers
+  //     subject: subject, // Subject line
+  //     text: text, // plain text body
+  //     html: html, // html body
+  //   });
+  // };
+
+  // var nodemailer = require("nodemailer");
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-      user: "agrawalmohit.tech@gmail.com", // generated ethereal user
-      pass: "HLn9YOswp2Wx85C4", // generated ethereal password
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASSWORD,
     },
   });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: `inShare <${from}>`, // sender address
+  var mailOptions = {
+    from: process.env.MAIL_USER,
     to: to, // list of receivers
     subject: subject, // Subject line
     text: text, // plain text body
     html: html, // html body
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
   });
+  // module.exports = sendMail;
 };
