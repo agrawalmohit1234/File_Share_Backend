89be59d1dd9c7a08582255d95ebc0266eb3c9d9a
unknown <rajagrawalmohit2@gmail.com>
2021-09-27 17:43:02 +0530

    Add Cors

--- server.js
+++ server.js
@@ -24,6 +24,16 @@ const corsOptions = {
 //   }
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.set({
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers":
+      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+  });
+
+  next();
+});
 app.use(express.static("public"));
 
 app.use(express.json());
